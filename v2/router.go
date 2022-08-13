6782ed017874891de6392822fccdda0a6026a72a
alien <13373935140@163.com>
2022-08-13 23:32:49 +0800

    v2版本 简洁化使用gin 使用router

--- v2/router.go
+++ v2/router.go
@@ -11,6 +11,12 @@ func NewRouter(options HttpConfig) *HttpEngine {
 	for _, handlerType := range options.GlobalMiddleware {
 		r.Use(NewHandler(handlerType))
 	}
+	if len(options.Host) == 0 {
+		options.Host = "0.0.0.0"
+	}
+	if options.Port == 0 {
+		options.Port = 9901
+	}
 	return &HttpEngine{
 		Router:  r,
 		Options: options,
