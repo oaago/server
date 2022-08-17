ef77c3906cb217d5f34eeacad102fbc896b93c1a
alien <13373935140@163.com>
2022-08-17 22:32:23 +0800

    增加 修改翻译

---
+++ v2/translator/translator.go
@@ -0,0 +1,28 @@
+package translator
+
+import (
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	zhs "github.com/go-playground/validator/v10/translations/zh"
+)
+
+var (
+	validate = validator.New()          // 实例化验证器
+	chinese  = zh.New()                 // 获取中文翻译器
+	uni      = ut.New(chinese, chinese) // 设置成中文翻译器
+	trans, _ = uni.GetTranslator("zh")  // 获取翻译字典
+)
+
+// InitTrans 初始化翻译器
+func InitTrans(p interface{}) interface{} {
+	_ = zhs.RegisterDefaultTranslations(validate, trans)
+	validate := validator.New()
+	err := validate.Struct(p)
+	if err != nil {
+		if errors, ok := err.(validator.ValidationErrors); ok {
+			return errors.Translate(trans)
+		}
+	}
+	return nil
+}
