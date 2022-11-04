e1c808e7ea12528c47d2150fe8105b1b8fa02ca3
alien <13373935140@163.com>
2022-11-04 11:17:00 +0800

    增加跨域

--- v2/socket/socket.go
+++ v2/socket/socket.go
@@ -5,6 +5,7 @@ import (
 	socketio "github.com/googollee/go-socket.io"
 	"github.com/oaago/cloud/logx"
 	"github.com/oaago/cloud/op"
+	"github.com/oaago/server/v2/http/middlewares/cors"
 	"github.com/olahol/melody"
 )
 
@@ -15,15 +16,15 @@ func InitSocket(r *gin.Engine) {
 	if op.ConfigData.Socket.Types == "socketio" {
 		baseUrl := op.ConfigData.Socket.BaseUrl
 		socket := socketio.NewServer(nil)
-		r.GET(baseUrl, gin.WrapH(socket))
-		r.POST(baseUrl, gin.WrapH(socket))
+		r.GET(baseUrl, cors.Cors("*"), gin.WrapH(socket))
+		r.POST(baseUrl, cors.Cors("*"), gin.WrapH(socket))
 	} else if op.ConfigData.Socket.Types == "ws" {
 		baseUrl := op.ConfigData.Socket.BaseUrl
 		m := melody.New()
-		r.GET(baseUrl, func(c *gin.Context) {
+		r.GET(baseUrl, cors.Cors("*"), func(c *gin.Context) {
 			m.HandleRequest(c.Writer, c.Request)
 		})
-		r.POST(baseUrl, func(c *gin.Context) {
+		r.POST(baseUrl, cors.Cors("*"), func(c *gin.Context) {
 			m.HandleRequest(c.Writer, c.Request)
 		})
 	} else {
